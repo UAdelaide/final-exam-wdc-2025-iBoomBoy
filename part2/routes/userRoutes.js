fa442cf48d86c83d229e23389ed8c73ba01e6eb8
iBoomBoy <ajaygramesh@icloud.com>
2025-06-20 13:30:57 +0000

    [automatic commit] 2025-06-20 13:30:55 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -40,7 +40,7 @@ router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   console.log('Login attempt with:', req.body);
-  
+
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
