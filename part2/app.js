fd4ff91d9ec8ab397d017b942d19e5f398970678
iBoomBoy <ajaygramesh@icloud.com>
2025-06-20 13:45:43 +0000

    [automatic commit] 2025-06-20 13:45:43 UTC

--- part2/app.js
+++ part2/app.js
@@ -9,13 +9,19 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(session({
+  secret: 'dogwalk_secret', // use env var in real apps
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false } // set to true only if HTTPS
+}));
+
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-
 
 
 // Export the app instead of listening here
