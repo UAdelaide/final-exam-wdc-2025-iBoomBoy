f6e5fba98908198f1834b954e7fae27fb93b3a94
iBoomBoy <ajaygramesh@icloud.com>
2025-06-20 12:50:51 +0000

    [automatic commit] 2025-06-20 12:50:50 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -55,4 +55,15 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// POST logout: destroy session
+router.post('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      return res.status(500).json({ error: 'Logout failed' });
+    }
+    res.clearCookie('connect.sid'); // clear session cookie
+    res.json({ message: 'Logged out' });
+  });
+});
+
 module.exports = router;
