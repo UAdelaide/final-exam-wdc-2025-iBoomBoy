cb3bd5dca4e19b93ad67b10b56cdd82c55e29e82
iBoomBoy <ajaygramesh@icloud.com>
2025-06-20 15:09:03 +0000

    [automatic commit] 2025-06-20 15:09:03 UTC

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const session = require('express-session');
+const session = require('express-session'); // Question 13: Enabled session tracking
 require('dotenv').config();
 
 const app = express();
